4e455059d746a4a0da59cea9aa79470ce079dd4f
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-03 19:42:07 +0900

    activehashの修正およびnewアクションの設定

--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,4 +1,4 @@
-class Category < ActiveHash::Base
+class ShippingDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '---'},
     { id: 2, name: '1～2日で発送'},
@@ -6,8 +6,7 @@ class Category < ActiveHash::Base
     { id: 4, name: '4～7日で発送'},
 ]
 
-end
-
 include ActiveHash::Associations
 has_many :products
+
 end
