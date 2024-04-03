0c8a323c79c737c67c936f7bf10a972ad4c27f35
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-04 00:41:38 +0900

    activehashの修正及びtax.jsの実装

---
+++ app/javascript/tax.js
@@ -0,0 +1,15 @@
+function tax () {
+  const itemPrice = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  itemPrice.addEventListener('input',() => {
+    const price = itemPrice.value;
+    const taxPrice = Math.floor(price * 0.1);
+    const salesProfit = Math.floor(price - taxPrice);
+
+    addTaxPrice.innerHTML = taxPrice;
+    profit.innerHTML = salesProfit;
+  });
+}
+window.addEventListener("load", tax);
