06061f6b57483e10d5743d7c5df4cf189d76a027
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-08 18:42:40 +0900

    productコントローラーのcreateアクション内のコード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to '/'
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
