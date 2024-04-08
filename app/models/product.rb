264551a768fc0136bc0623a7b4aa9068b1ec6d8d
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-08 19:47:10 +0900

    バリデーションの修正①

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,17 +13,19 @@ class Product < ApplicationRecord
     validates :image, presence: true, unless: :was_attached?
     validates :name
     validates :describe
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_day_id
     validates :product_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    with_options numericality: { other_than: 1, message: "can't be blank" } do
-      validates :category_id
-      validates :status_id
-      validates :shipping_cost_id
-      validates :prefecture_id
-      validates :shipping_day_id
-    end
   end
+end
+  
+  validates :product_price, format: { with: /\A[0-9]+\z/ }
 
-  def was_attached?
-    image.attached?
-  end
+    def was_attached?
+      image.attached?
+    end
 end
