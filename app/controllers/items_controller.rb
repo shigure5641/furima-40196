94f0d4a40debae8a2527d65d63a6aa2ed27c009f
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-14 01:40:19 +0900

    商品編集画面への遷移条件を追加(商品作成者以外が遷移できない様に指定)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,10 @@ class ItemsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    unless @product.user == current_user
+      redirect_to root_path
+    end 
+
   end
 
   def update
