5bdc613b96b3c92c35a68deb2544e268fcd3c256
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-14 01:52:32 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @products = Product.all.order('created_at DESC')
