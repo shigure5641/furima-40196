139b725caac12b44544528fb50d56d21dec192d7
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-25 19:01:20 +0900

    フォームオブジェクトの作成

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,22 +1,21 @@
-class Order_Form
+class OrderForm
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, :municipalities, :street_address,
-  :building_name, :telephone_number, :user_id, :product_id, :token
+  :building_name, :telephone_number, :user_id, :product_id
+
   with_options presence: true do
-    validates :postcode
-    validates :prefecture_id
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :municipalities
     validates :street_address
-    validates :telephone_number
-    validates :token
+    validates :telephone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
     validates :user_id
     validates :product_id
   end
 
   def save
     order = Order.create(user_id: user_id, product_id: product_id)
-    shipping_address.create(order_id: order_id, postcode: postcode, prefecture_id: prefecture_id, municipalities,municipalities,
-    street_address: shipping_address, building_name: building_name, telephone_number: telephone_number)
-    
+    ShippingAddress.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, municipalities: municipalities,
+    street_address: street_address, building_name: building_name, telephone_number: telephone_number)
   end
 end
