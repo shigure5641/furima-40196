8a114c561f0bf4a8d38db1049effc815f5da4c9c
shigure5641 <154601291+shigure5641@users.noreply.github.com>
2024-04-25 19:01:56 +0900

    ordersコントローラーの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
+
+
+  def index
+    @order_form = OrderForm.new
+end
+
+def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+end
+
+  private
+
+  def order_params
+    binding.pry
+    params.require(:order_form).permit(:postcode, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id, product_id: params[:product_id])
+  end
+
+  def non_purchased_item
+    @item = Product.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
 end
